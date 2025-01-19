732bf2d6c476a1c5922bc959a3f58e7df007bfbf
Aditya Latta <adityalatta78@gmail.com>
2025-01-19 23:01:34 +0530

    Added retries to axios get request error

--- src/context/CountryContext.jsx
+++ src/context/CountryContext.jsx
@@ -58,8 +58,33 @@ export const CountryProvider = ({ children }) => {
     useEffect(() => {
         const getCountries = async () => {
             try {
+                axios.interceptors.response.use(undefined, (err) => {
+                    const { config, message } = err;
+                    if (!config || !config.retry) {
+                        return Promise.reject(err);
+                    }
+                    // retry while Network timeout or Network Error
+                    if (
+                        !(
+                            message.includes("timeout") ||
+                            message.includes("Network Error")
+                        )
+                    ) {
+                        return Promise.reject(err);
+                    }
+                    config.retry -= 1;
+                    const delayRetryRequest = new Promise((resolve) => {
+                        setTimeout(() => {
+                            console.log("retry the request", config.url);
+                            resolve();
+                        }, config.retryDelay || 1000);
+                    });
+                    return delayRetryRequest.then(() => axios(config));
+                });
+
                 const res = await axios.get(
-                    "https://restcountries.com/v3.1/all"
+                    "https://restcountries.com/v3.1/all",
+                    { retry: 3, retryDelay: 3000 }
                 );
 
                 setAllCountries(res.data);
